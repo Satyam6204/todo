4a3326b4f102d7f18fa97b5bc092b4232ef0c032
Satyam6204 <satu45025@gmai.com>
2025-01-26 17:31:44 +0530

    add time update

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+
 let count = 1;
-MAX_VALUE = 7;
+console.log(count);
+MAX_VALUE = 8;
 const fun = () => {
 
     let box2 = document.getElementById('box2')
@@ -12,18 +14,24 @@ const fun = () => {
     } else {
 
         if (count == MAX_VALUE) {
+
             alert("box is full")
         } else {
+
             ////////////////////////////////////
             let div = document.createElement('div');
             div.classList.add("div1");
             const closeText = () => {
                 text2.remove();
                 Cimg.remove();
-                count = 1;
+                count--;
+                console.log(count)
             }
+            let timeBag = new Date();
+            let hours = timeBag.getHours()
+            let min = timeBag.getMinutes()
+            let time = `${hours}:${min}`;
             div.addEventListener('click', closeText);
-
             ////////////////////////////////////
             let text2 = document.createElement("h3");
             let Cimg = document.createElement("img");
@@ -31,14 +39,15 @@ const fun = () => {
             Cimg.style.width = "20px";
             text2.style.backgroundColor = "yellow";
             text2.style.borderRadius = "5px"
-            text2.style.height = "4vmin"
+            text2.style.height = "3.5vmin"
             text2.style.marginTop = "6px"
-            text2.style.fontSize = "3vmin"
+            text2.style.fontSize = "2vmin"
             text2.style.width = "100%"
-            text2.textContent = `${count}. ${text}`;
+            text2.textContent = `(Time:${time}) ${text}`;
             div.append(text2, Cimg);
             box2.appendChild(div);
             count++;
+            console.log(count)
 
         }
     }
